53571e3f95c1a5c5af57167bc410c4f4253b25cb
Samuel Ralak <thesamuelralak@gmail.com>
2025-11-25 01:49:17 +0300

    create db on boot if not exists

--- apps/argus/src/database/database.module.ts
+++ apps/argus/src/database/database.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { DataSourceOptions } from 'typeorm';
+import { createDatabase } from 'typeorm-extension';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: config.get<number>('DB_PORT', 5432),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: config.get('NODE_ENV') !== 'production',
-      }),
+      useFactory: async (config: ConfigService) => {
+        const options: DataSourceOptions = {
+          type: 'postgres',
+          host: config.get('DB_HOST'),
+          port: config.get<number>('DB_PORT', 5432),
+          username: config.get('DB_USERNAME'),
+          password: config.get('DB_PASSWORD'),
+          database: config.get('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: config.get('NODE_ENV') !== 'production',
+        };
+
+        await createDatabase({
+          options,
+          ifNotExist: true,
+          initialDatabase: 'postgres',
+        });
+
+        return options;
+      },
       inject: [ConfigService],
     }),
   ],

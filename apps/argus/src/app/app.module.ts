1313c40b7e674a215c57fd170df1edc39dba1df1
Samuel Ralak <thesamuelralak@gmail.com>
2025-11-25 19:48:46 +0300

    Remove default scaffolding controller and service from argus application

--- apps/argus/src/app/app.module.ts
+++ apps/argus/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from '@opareta/common';
 import { DatabaseModule, databaseConfig } from './database';
 import { AuthModule } from './auth';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -17,7 +15,5 @@ import { AppService } from './app.service';
     CommonModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
